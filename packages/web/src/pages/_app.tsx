2d19eaa4946c95b0f5fa862dc31cf5e515813c78
Jory Tindall <jorytindall@gmail.com>
2022-08-07 18:34:32 -0700

    Added Fathom analytics

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -1,6 +1,28 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }) {
+	const router = useRouter()
+
+	// Fathom Analytics
+	useEffect(() => {
+		Fathom.load(process.env.FATHOM_SITE_ID, {
+			includedDomains: [ 'jorytindall.com' ]
+		})
+
+		function onRouteChangeComplete() {
+			Fathom.trackPageview();
+		}
+
+		router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+		return () => {
+			router.events.off('routeChangeComplete', onRouteChangeComplete);
+		}
+	}, [])
+
 	return <Component {...pageProps} />;
 }
 
