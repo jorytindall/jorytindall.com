b3db78f4d387bfbdff92e6dc899016d2701fb37b
Jory Tindall <jorytindall@gmail.com>
2022-08-08 23:10:41 -0700

    Built type tokens, badge component

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<div>
 			<p>Hello, world!</p>
-			<Badge text="Badge text" type='primary' />
+			<Badge text="Badge text" type='secondary' />
 		</div>
 	);
 }
