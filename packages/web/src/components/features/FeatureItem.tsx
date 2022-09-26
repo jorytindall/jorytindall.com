ef540a762c6d6c1958c9d51db85401ed4d44e9fa
Jory Tindall <jory.tindall@hashicorp.com>
2022-09-25 23:29:29 -0700

    Cleaning up feature items

---
+++ packages/web/src/components/features/FeatureItem.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import { useNextSanityImage } from "next-sanity-image"
+import { sanityClient } from 'lib/sanity.server'
+import { Container } from "components/layout"
+import { Headline, Paragraph } from 'components/typography'
+import styles from 'styles/components/Features.module.scss'
+
+interface FeatureItemProps {
+    image: string,
+    title: string,
+    description: string,
+}
+
+export const FeatureItem = ({ image, title, description }: FeatureItemProps) => {
+
+    const imageProps = useNextSanityImage(
+        sanityClient,
+        image,        
+    )
+
+    return (
+        <>
+            <Container
+                isFlex
+                flexDirection='column'
+                semanticElement='div'
+                density='collapse'
+            >
+                {image && <Image {...imageProps} layout='intrinsic' />}
+                {title && <Headline type='h5'>{title}</Headline>}
+                {description && <Paragraph type='primary' collapse>{description}</Paragraph>}
+            </Container>
+        </>
+    )
+}
---
+++ packages/web/src/components/features/Features.tsx
@@ -0,0 +1,35 @@
+import styles from 'styles/components/Features.module.scss'
+import { FeatureItem } from './FeatureItem'
+
+interface FeaturesProps {
+    input: {
+        featureItems: [
+            title: string,
+            image: any,
+            alternativeText: string
+        ]
+        _key: string,
+    }
+}
+
+export const Features = ({ input }:FeaturesProps) => {
+
+    const { featureItems, _key } = input;
+
+    const mapFeatures = featureItems.map(item => {
+        return (
+            <FeatureItem
+                image={item.image}
+                title={item.title}
+                description={item.description}
+                key={item.title}
+            />
+        )
+    })
+
+    return (
+        <section className={styles.wrapper}>
+            {mapFeatures}
+        </section>
+    )
+}
