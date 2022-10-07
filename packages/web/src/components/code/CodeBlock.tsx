1fbe33ed95b76674c6640dfad6fec874ae4215b4
Jory Tindall <jory@hypecreativestudios.com>
2022-10-07 20:11:19 +0000

    Updating styling of the code block and adding a theme.

--- packages/web/src/components/code/CodeBlock.tsx
+++ packages/web/src/components/code/CodeBlock.tsx
@@ -1,31 +1,38 @@
-import SyntaxHighlighter from 'react-syntax-highlighter'
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import styles from 'styles/components/CodeBlock.module.scss'
-import { Paragraph } from 'components/typography'
-import { getClasses } from 'utils/getClasses'
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { Paragraph } from 'components/typography';
+import { getClasses } from 'utils/getClasses';
+import styles from 'styles/components/CodeBlock.module.scss';
 
 interface CodeProps {
-    code: string,
-    description?: string,
-    language: string,
+	code: string;
+	description?: string;
+	language: string;
 }
 
 export const CodeBlock = ({ code, description, language }: CodeProps) => {
+	const classes = getClasses([styles.wrapper]);
 
-    const classes = getClasses([
-        styles.wrapper
-    ])
+	const codeWrapperStyles = {
+		padding: '1.5rem',
+		borderRadius: '10px',
+		boxShadow: '0px 0px 24px 0px rgba(0,0,0,0.05)',
+	};
 
-    return (
-        <section className={classes}>
-            <SyntaxHighlighter
-                language={language}
-            >
-                {code}
-            </SyntaxHighlighter>
-            {description && (
-                <Paragraph type='secondary' collapse>{description}</Paragraph>
-            )}
-        </section>
-    )
-}
+	return (
+		<section className={classes}>
+			<SyntaxHighlighter
+				language={language}
+				style={atomOneDark}
+				customStyle={codeWrapperStyles}
+			>
+				{code}
+			</SyntaxHighlighter>
+			{description && (
+				<Paragraph type="secondary" collapse>
+					{description}
+				</Paragraph>
+			)}
+		</section>
+	);
+};
