5f3a0c8be537f6650a1b471a0d431add4990fae4
Jory Tindall <jory@hypecreativestudios.com>
2022-10-09 19:42:12 +0000

    Update 24 files

--- packages/web/src/utils/datetimeFormat.ts
+++ packages/web/src/utils/datetimeFormat.ts
@@ -1,13 +1,13 @@
-import { format, parseISO } from 'date-fns'
+import { format, parseISO } from 'date-fns';
 
 export const formatIso = (date: string) => parseISO(date);
 
 export const formatDateTime = (dateIso: string) => {
-    const formatted = format(formatIso(dateIso), 'PPPp');
-    return formatted;
-}
+	const formatted = format(formatIso(dateIso), 'PPPp');
+	return formatted;
+};
 
 export const formatDateString = (dateIso: string) => {
-    const formatted = format(formatIso(dateIso), 'ddMMYYYY');
-    return formatted;
-}
+	const formatted = format(formatIso(dateIso), 'ddMMYYYY');
+	return formatted;
+};
