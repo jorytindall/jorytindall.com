3e67e2052af2624b8b38685334fcf755d75636bc
Jory Tindall <jory.tindall@hashicorp.com>
2023-06-14 13:53:12 -0700

    Updating groq query and adding a list of projects to the page

--- packages/web/src/lib/queries/music.ts
+++ packages/web/src/lib/queries/music.ts
@@ -18,3 +18,12 @@ export const GET_MUSIC_PROJECTS = groq`
 export const GET_MUSIC_PROJECT_PATHS = groq`
     *[_type == 'musicProject' && defined(slug.current)][].slug.current
 `;
+
+export const GET_ALL_MUSIC_PROJECTS = groq`
+    *[_type == 'musicProject'][] {
+        _id,
+        title,
+        description,
+        "slug": slug.current,
+    }
+`
--- packages/web/src/pages/music/index.tsx
+++ packages/web/src/pages/music/index.tsx
@@ -1,10 +1,47 @@
+import Link from 'next/link'
+import { GET_ALL_MUSIC_PROJECTS } from "lib/queries";
+import { getClient } from 'lib/sanity.server';
+import { linkResolver } from "utils/linkResolver";
 import { Layout } from "components/layout";
 import { PageTitle } from "components/page-title";
+import { MetaHead } from "components/meta";
 
-export default function MusicPage() {
+export default function MusicPage({ data }) {
+
+  console.log(data)
+
+  const musicProjects = data.musicProjects.map(musicProject => {
+    return (
+      <Link
+        href={linkResolver('musicProject', musicProject.slug)}
+        key={musicProject._id}
+      >
+        {musicProject.title}
+      </Link>
+    )
+  })
+
   return (
     <Layout>
+      <MetaHead
+				title="Music"
+				description="Music projects, ensembles, and current working groups."
+				slug="music"
+			/>
       <PageTitle title="🎵 Music Projects" megaTitle="Music" />
+      {musicProjects}
     </Layout>
   )
+}
+
+export async function getStaticProps({ preview = false }) {
+	const musicProjects = await getClient(preview).fetch(GET_ALL_MUSIC_PROJECTS);
+
+	return {
+		props: {
+			preview,
+			data: { musicProjects },
+		},
+		revalidate: 60,
+	};
 }
