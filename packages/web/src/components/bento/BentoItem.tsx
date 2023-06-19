41ac1f51d9cec326e2a99e778056032cfd0a05aa
Jory Tindall <jory.tindall@hashicorp.com>
2023-06-19 14:15:30 -0700

    Adding Bento components and scaffolding out homepage

---
+++ packages/web/src/components/bento/BentoItem.tsx
@@ -0,0 +1,36 @@
+import { getClasses } from 'utils/getClasses'
+import styles from 'styles/components/bento/BentoItem.module.scss'
+
+interface BentoItemProps {
+  children: React.ReactNode,
+  background?: string,
+  className?: string,
+  size: string,
+  padding: string,
+  gap: string,
+}
+
+export const BentoItem = ({
+  children,
+  background,
+  className,
+  size,
+  padding,
+  gap,
+}: BentoItemProps) => {
+
+  const classes = getClasses([
+    styles.wrapper,
+    styles[background],
+    styles[size],
+    styles[`padding--${padding}`],
+    styles[`gap--${gap}`],
+    className,
+  ])
+
+  return (
+    <article className={classes}>
+      {children}
+    </article>
+  )
+}
--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Headline, Paragraph, InlineLink } from 'components/typography';
 import { TextArrow } from 'components/button';
 import { Layout } from 'components/layout'
 import styles from 'styles/pages/Home.module.scss';
+import { BentoBox,BentoItem } from 'components/bento';
 
 export default function Home() {
 	return (
@@ -11,7 +12,96 @@ export default function Home() {
 				title="Designer, saxophonist artist, and educator"
 				description="Home of Jory Tindall; User experience designer, saxophone artist, and music educator based in Seattle, Washington."
 			/>
-			<section className={styles.mainContent}>
+			<BentoBox>
+				<BentoItem
+					size='eight'
+					background='tertiary'
+					padding='x-large'
+					gap='large'
+				>
+					<Headline tag="h1" size='h2' color='primary' collapse>
+							Hey 🤘 My name is Jory Tindall.
+					</Headline>
+					<Headline tag="p" size='h4' color='primary' collapse>
+						I&apos;m an empathetic interface and experience designer
+						with passions for system-thinking and design systems,
+						design technology, and shredding the saxophone,
+						currently based in Seattle, Washington.
+					</Headline>
+					<Paragraph collapse>
+						I&apos;m currently helping to empower a broad range of
+						designers and creative thinkers on the Design System
+						team at{' '}
+						<InlineLink
+							type="external"
+							target="_blank"
+							href="https://hashicorp.com"
+						>
+							HashiCorp,
+						</InlineLink>{' '}
+						previously at the{' '}
+						<InlineLink
+							type="external"
+							target="_blank"
+							href="https://t-mobile.com"
+						>
+							T-Mobile Design System team.
+						</InlineLink>
+					</Paragraph>
+					<Paragraph collapse>
+						You can also find me leading the charge to democratize
+						music education at{' '}
+						<InlineLink
+							href="https://downbeatacademy.com"
+							type="external"
+							target="_blank"
+						>
+							Downbeat Academy
+						</InlineLink>
+						, collaborating in the Seattle music scene, and teaching
+						teaching music to the next generation of shredders.
+					</Paragraph>
+				</BentoItem>
+				<BentoItem
+					size="four"
+					padding='x-large'
+					gap='large'
+					background='dark'
+				>
+					<Headline tag="h2" size="h4" color='light' collapse>Upcoming events</Headline>
+				</BentoItem>
+				<BentoItem
+					size='four'
+					padding='x-large'
+					gap='small'
+					background='tertiary'
+				>
+					<Headline tag='h3' size='h4' color='primary' collapse>Portfolio</Headline>
+					<Paragraph type='secondary' collapse>Recent work in design, engineering, and other creative projects.</Paragraph>
+					<TextArrow href='/portfolio'>My portfolio</TextArrow>
+				</BentoItem>
+				<BentoItem
+					size='four'
+					padding='x-large'
+					gap='small'
+					background='tertiary'
+				>
+					<Headline tag='h3' size='h4' color='primary' collapse>Music</Headline>
+					<Paragraph type='secondary' collapse>Music projects, original compositions, current working ensembles.</Paragraph>
+					<TextArrow href='/music'>My music</TextArrow>
+				</BentoItem>
+				<BentoItem
+					size='four'
+					padding='x-large'
+					gap='small'
+					background='secondary'
+				>
+					<Headline tag='h3' size='h4' color='dark' collapse>Get in touch</Headline>
+					<Paragraph type='secondary' collapse>Reach out to say hello 👋!</Paragraph>
+					<TextArrow href='/contact'>Contact me</TextArrow>
+				</BentoItem>
+			</BentoBox>
+			{/* <section className={styles.mainContent}>
 				<article>
 					<Headline tag="h1" size='h2'>
 						Hey 🤘 My name is Jory Tindall.
@@ -68,7 +158,7 @@ export default function Home() {
 						new collaborations.
 					</Paragraph>
 				</article>
-			</section>
+			</section> */}
 		</Layout>
 	);
 }
