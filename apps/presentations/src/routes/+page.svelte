58c1056cd8a8d1ea65591bed07a0abd700b176bd
Jory Tindall <jory.tindall@hashicorp.com>
2023-11-23 11:42:18 -0800

    Added some basic styling to the home page

--- apps/presentations/src/routes/+page.svelte
+++ apps/presentations/src/routes/+page.svelte
@@ -1,8 +1,23 @@
+<script>
+	import '../styles/home.css';
+</script>
+
 <svelte:head>
 	<title>Jory's Presentations</title>
 </svelte:head>
 
-<h1>Jory's presentations</h1>
+<main>
+	<section class="container">
+		<article class="presentations">
+			<h1>Jory's presentations</h1>
 
-<a href="/design-for-your-engineer-part-one">Design For Your Engineer: Part I</a
->
+			<a href="/design-for-your-engineer-part-one"
+				>Design For Your Engineer: Part I</a
+			>
+		</article>
+
+		<a href="https://jorytindall.com" target="blank" class="back-link"
+			>⬅️ Back to jorytindall.com</a
+		>
+	</section>
+</main>
