80519722cdebd6a4f7811f543b15d750b57d09ba
Jory Tindall <jorytindall@gmail.com>
2023-12-27 14:40:39 -1000

    Migrated events to app router

---
+++ apps/web/src/app/(pages)/events/page.tsx
@@ -0,0 +1,53 @@
+import { sanityClient } from "utils/sanity/sanityClient";
+import { format, parseISO } from "date-fns";
+import { GET_ALL_EVENTS } from "lib/queries";
+import { getCurrentEvents } from "utils/getCurrentEvents";
+import { PageTitle } from "components/page-title";
+import { Paragraph } from "components/typography";
+import { CenteredWrapper } from "components/layout";
+import { ListItem } from "components/list";
+import { linkResolver } from "utils/linkResolver";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Events | Jory Tindall',
+    description: 'Upcoming events and live music',
+}
+
+export default async function Events() {
+
+    const client = sanityClient
+    const events = await client.fetch(GET_ALL_EVENTS, {
+        next: {
+            revalidate: 60,
+        },
+    })
+
+    const currentEvents = getCurrentEvents(events);
+
+    const filterEvents =
+        currentEvents.length > 0 ? (
+            currentEvents.map((event) => {
+                return (
+                    <ListItem
+                        title={event.title}
+                        link={linkResolver('event', event.slug)}
+                        key={event._id}
+                    >
+                        <Paragraph type='primary' collapse><strong>Location: </strong>{event.location}</Paragraph>
+                        <Paragraph type='primary' collapse><strong>Date: </strong>{format(parseISO(event.date), 'MMMM do, yyyy')}</Paragraph>
+                    </ListItem>
+                );
+            })
+        ) : (
+            <Paragraph>No upcoming events, check back soon!</Paragraph>
+        );
+
+    return (
+        <>
+            <PageTitle title="🗓 Events" megaTitle="Upcoming Events" />
+            <CenteredWrapper semanticElement="section">{filterEvents}</CenteredWrapper>
+        </>
+    );
+}
