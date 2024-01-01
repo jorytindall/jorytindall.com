dcda40b43a240c5dc3956f29856766965f1a644f
Jory Tindall <jorytindall@gmail.com>
2024-01-01 13:02:46 -0800

    Migrated music projects to bento

--- apps/web/src/app/(pages)/music/page.tsx
+++ apps/web/src/app/(pages)/music/page.tsx
@@ -2,7 +2,10 @@ import { sanityClient } from 'lib/sanity/sanityClient';
 import { GET_ALL_MUSIC_PROJECTS } from 'lib/queries';
 import { PageTitle } from 'app/components/page-title';
 import styles from 'styles/pages/Music.module.scss';
-import { MusicItem } from 'app/components/music';
+import { linkResolver } from 'utils/linkResolver';
+import { getSanityImageUrl } from 'utils/getSanityImage';
+import { BentoBox, BentoItem } from 'app/components/bento';
+import { Headline, Paragraph } from 'app/components/typography';
 
 import type { Metadata } from 'next';
 
@@ -15,21 +18,47 @@ export default async function MusicPage() {
 	const musicProjects = await sanityClient.fetch(GET_ALL_MUSIC_PROJECTS);
 
 	const musicItems = musicProjects.map((musicProject) => {
+
+		const { title, slug, description } = musicProject;
+		const image = getSanityImageUrl(musicProject.image.asset._ref);
+		const link = linkResolver('musicProject', slug);
+
+
 		return (
-			<MusicItem
-				link={musicProject.slug}
-				title={musicProject.title}
-				description={musicProject.description}
-				image={musicProject.image}
-				key={musicProject._id}
-			/>
+			// <MusicItem
+			// 	link={musicProject.slug}
+			// 	title={musicProject.title}
+			// 	description={musicProject.description}
+			// 	image={musicProject.image}
+			// 	key={musicProject._id}
+			// />
+			<BentoItem
+				key={title}
+				background='tertiary'
+				size='medium'
+				padding='large'
+				gap='default'
+				isInteractive={true}
+				href={link}
+				image={image}
+				imagePosition="top"
+			>
+				<Headline tag='h2' size='h4' color='secondary' collapse>{musicProject.title}</Headline>
+				<Paragraph>{description}</Paragraph>
+			</BentoItem>
 		);
 	});
 
 	return (
 		<>
 			<PageTitle title="🎵 Music" megaTitle="Music projects" />
-			<section className={styles.outer}>{musicItems}</section>
+			<BentoBox
+				className={styles.wrapper}
+				isFullBleed={false}
+			>
+				{musicItems}
+			</BentoBox>
+			{/* <section className={styles.outer}>{musicItems}</section> */}
 		</>
 	);
 }
