f8306f48a7611c4374159b1de9a31926a9c68646
Jory Tindall <jory.tindall@hashicorp.com>
2024-11-26 23:53:32 -0800

    Migrate Layout components to CSS

--- apps/web/src/components/layout/Main.tsx
+++ apps/web/src/components/layout/Main.tsx
@@ -1,4 +1,4 @@
-import styles from 'styles/components/layout/Main.module.scss';
+import styles from './Main.module.css';
 
 export const Main = ({ children }: { children: React.ReactNode }) => {
   return <main className={styles.main}>{children}</main>;
