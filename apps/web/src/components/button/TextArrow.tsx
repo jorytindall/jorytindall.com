e291d50885c23d195fd6b197a7b02f3ed57bf21e
Jory Tindall <jory.tindall@hashicorp.com>
2024-11-26 23:41:58 -0800

    Cleanup and removal of unused SCSS

--- apps/web/src/components/button/TextArrow.tsx
+++ apps/web/src/components/button/TextArrow.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-import styles from './TextArrow.module.css';
 import { getClasses } from 'utils/getClasses';
+import styles from './TextArrow.module.css';
 
 interface TextArrowProps {
 	href?: string;
