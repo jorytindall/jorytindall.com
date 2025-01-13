d41b4af0d1db534aecbf58fa84709b9c2faf77c9
Jory Tindall <jory.tindall@hashicorp.com>
2025-01-12 22:47:07 -0800

    Adding TZDate

--- apps/web/src/utils/datetimeFormat.ts
+++ apps/web/src/utils/datetimeFormat.ts
@@ -1,6 +1,6 @@
 import { format, parseISO } from 'date-fns';
 import { toZonedTime } from 'date-fns-tz';
-import { tz } from '@date-fns/tz';
+import { TZDate } from '@date-fns/tz';
 
 // Helper function to convert to specified timezone
 const toSpecifiedTimezone = (date: string | Date, timeZone: string): Date => {
@@ -11,5 +11,6 @@ const toSpecifiedTimezone = (date: string | Date, timeZone: string): Date => {
 // Catch-all function to format the date, time, and timezone
 export const formatDate = (dateIso: string, dateFormat: string, timeZone: string) => {
   // const zonedDate = toSpecifiedTimezone(dateIso, );
-  return format(dateIso, dateFormat, { in: tz(timeZone)});
+  // console.log(format(dateIso, dateFormat, { in: tz(timeZone)}));
+  return format(new TZDate(dateIso, timeZone), dateFormat);
 }
