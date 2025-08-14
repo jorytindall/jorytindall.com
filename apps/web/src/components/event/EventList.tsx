1241934e82ce2372d4db6d24f1b2265ba38ededd
Jory Tindall <jory@hypecreativestudios.com>
2025-08-14 00:04:28 -0700

    Updated styling for event list items

--- apps/web/src/components/event/EventList.tsx
+++ apps/web/src/components/event/EventList.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { linkResolver } from 'utils/linkResolver';
-import { formatDate, formatEventDate, formatEventDateTime } from 'utils/datetimeFormat';
+import { formatEventDateTime } from 'utils/datetimeFormat';
 import styles from './EventList.module.css';
 
 export const EventList = ({ events }) => {
@@ -10,13 +10,6 @@ export const EventList = ({ events }) => {
 		<div className={styles.wrapper}>
 			{events.length > 0 ? (
 				events.map((event) => {
-					// Debug logging to verify timezone conversion
-					console.log(`Event: ${event.title}`);
-					console.log(`UTC Date: ${event.date}`);
-					console.log(`Pacific Date: ${formatEventDate(event.date)}`);
-					console.log(`With Time: ${formatEventDateTime(event.date)}`);
-					console.log('---');
-
 					return (
 						<div className={styles.event} key={event._id}>
 							<Link
